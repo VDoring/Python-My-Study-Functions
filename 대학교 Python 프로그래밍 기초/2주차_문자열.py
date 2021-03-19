6751038f1aa82b11ed251321df0877c2b5752d98
Doring <hojun.dev@gmail.com>
2021-03-19 22:15:27 +0900

    문자열 및 전반적인 출력 방법

---
+++ 대학교 Python 프로그래밍 기초/2주차_문자열.py
@@ -0,0 +1,25 @@
+import sys
+
+print("ABC")
+print("A's C")
+print('A"BC')
+print("""ABC""")
+print('''ABC''')
+
+print('안녕' + '파이썬')
+print('10' + '20')
+print('10 '*20)
+
+x = "ABC"
+print(x)
+print('안녕' + ' ' + '파이썬')
+
+print(sys.maxsize)
+print(-sys.maxsize - 1)
+
+print(1, 0, -1, 0.1234)
+
+print(1+1)
+print(1-1)
+print(2*3)
+print(6/3)
