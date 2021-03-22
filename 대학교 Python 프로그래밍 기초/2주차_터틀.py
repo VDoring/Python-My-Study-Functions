6b5bff365a777b19eea705c3273ae88225347f73
Doring <hojun.dev@gmail.com>
2021-03-22 22:45:55 +0900

    거북이 움직이기(turtle)

---
+++ 대학교 Python 프로그래밍 기초/2주차_터틀.py
@@ -0,0 +1,21 @@
+import turtle
+import time
+
+def td():
+    time.sleep(0.7)
+
+turtle.shape("turtle")
+td()
+turtle.write(turtle.position())
+td()
+turtle.forward(50)
+td()
+turtle.write(turtle.position())
+td()
+turtle.left(90)
+td()
+turtle.forward(50)
+td()
+turtle.write(turtle.position())
+td()
+# backward()도 있다.
