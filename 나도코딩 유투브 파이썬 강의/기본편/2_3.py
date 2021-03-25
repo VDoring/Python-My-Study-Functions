f0c46644f4a5da489a10e9aee18edcbea3ac9bf0
Doring <hojun.dev@gmail.com>
2021-03-25 21:36:46 +0900

    Boolean(참/거짓)

---
+++ 나도코딩 유투브 파이썬 강의/기본편/2_3.py
@@ -0,0 +1,11 @@
+# https://www.youtube.com/watch?v=kWiCuklohdY&t=4s
+# boolean 자료형
+# 참/거짓
+
+print(5>10) # False
+print(5<10) # True
+print(True)
+print(False)
+print(not True) # False
+print(not False) # True
+print(not (5 > 10)) # True
