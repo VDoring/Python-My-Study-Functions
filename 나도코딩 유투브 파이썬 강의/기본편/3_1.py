2466d11636ffbfdfb93ba068c809fe289c189d2e
Doring <hojun.dev@gmail.com>
2021-04-11 19:24:50 +0900

    연산자

---
+++ 나도코딩 유투브 파이썬 강의/기본편/3_1.py
@@ -0,0 +1,24 @@
+# 연산자
+
+print(1+1) # 더하기
+print(3-2) # 빼기
+print(5*2) # 곱하기
+print(6/3) # 나누기
+
+print(2**3) #2^3. 승수
+print(5//3) # 5/3 몫 구하기
+print(5%3) # 5/3 나머지구하기
+
+print(3 + 4 == 7) # True
+
+print(1 != 3) # True
+print(not(1 != 3)) # False
+
+print((3 > 0) and (5 > 0)) # True
+print((3 > 0) & (5 > 0)) # True
+
+print((3 > 0) or (0 > 5)) # True
+print((3 > 0) | (0 > 5)) # True
+
+print(5 > 4 > 3) # True
+print(4 > 5 > 4) # False
