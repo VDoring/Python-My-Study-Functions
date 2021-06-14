7d88d740d9ec020093977f62eab3f4fee5b9ccff
Doring <d0211468@kowon.dongseo.ac.kr>
2021-06-14 23:56:16 +0900

    서식문자 추가 옵션 종류

---
+++ 대학교 Python 프로그래밍 기초/13주차_서식문자.py
@@ -0,0 +1,11 @@
+a = 10
+b = 3.546
+ch = 'a'
+st = 'abc'
+
+print('value = %d' %a) # value = 10
+print('value = %-4d,%4d' %(a,a)) # value = 10  ,  10
+print('value = %f' %b) # value = 3.546000
+print('value = %4.1f,%4.2f' %(b,b)) # value =  3.5,3.55
+print('value = %c' %ch) # value = a
+print('value = %s' %st) # value = abc
