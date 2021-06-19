8f4f9d37870f20c16cbb71d0922ba1c9fba84305
Doring <d0211468@kowon.dongseo.ac.kr>
2021-06-19 11:23:37 +0900

    ceil(), floor(), 딕셔너리 요소 출력

---
+++ 대학교 Python 프로그래밍 기초/기말고사 문제연습/기말최종연습_11.py
@@ -0,0 +1,14 @@
+import math
+
+a = 5.1
+b = 10.9
+
+ceil_a = math.ceil(a)
+print(ceil_a) # 6
+
+cell_b = math.floor(b)
+print(cell_b) # 10
+
+dictionary = {'a':'x', 'byte':'nyan', 'c':'z'}
+print(dictionary['a']) # x 출력
+print(dictionary['byte']) # nyan 출력
