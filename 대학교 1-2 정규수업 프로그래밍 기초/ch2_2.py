c581f71fcccde324f4f5dc7c37a6a2e75c6f24d4
Doring <d0211468@kowon.dongseo.ac.kr>
2021-09-21 16:49:05 +0900

    2주차 - 변수 기본 사용법 및 출력

---
+++ 대학교 1-2 정규수업 프로그래밍 기초/ch2_2.py
@@ -0,0 +1,37 @@
+print('ch2_2')
+
+# name, age, sex
+
+a = "홍길동"
+b = "20"
+c = "남자"
+name = "홍길동"
+age = "20"
+sex = "남자"
+
+# line 300
+print(a, b, c)
+print(name, age, sex)
+
+number_of_pictures = 10
+numberOfPictures = 10
+
+myNewCar = "Volvo"
+
+mynewcar = "Volvo"
+
+print("\n"*23)
+
+score = 10
+
+print("score :", score)
+
+score = score + 1
+
+print("score :", score)
+
+x = 100
+y = 200
+sum = x + y
+print("x=",x,", y=",y,"x+y=",sum)
+print("x는 "+str(x)+"이고 y는 "+str(y)+"일때 x+y는 "+str(sum)+"이다")
