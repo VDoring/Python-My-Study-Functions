4dc7c4a8d3bbfb072125e6e6b46422cbfeb0cacc
Doring <d0211468@kowon.dongseo.ac.kr>
2021-10-02 23:33:09 +0900

    정수형 문자형 연산 및 출력

---
+++ 대학교 1-2 정규수업 프로그래밍 기초/4_1.py
@@ -0,0 +1,19 @@
+point = 100
+print("point = ", point)
+
+pi = 3.14
+print("pi = ", pi)
+
+greeting = "Hello" # c, c++, c#, java
+print("greeting = ", greeting)
+
+greeting = 'Hello2'
+print("greeting = ", greeting)
+
+#정수형 연산
+print(100+200)
+
+#문자열 연산
+print("100"+"200")
+print('100'+'200')
+print('100'+"200")
