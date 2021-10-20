de0a4a0da2172736e6830c8222da8d13f0161de5
Doring <d0211468@kowon.dongseo.ac.kr>
2021-10-20 11:41:49 +0900

    리스트 선언 및 출력, 요소 추가하기

---
+++ 대학교 1-2 정규수업 프로그래밍 기초/4_3.py
@@ -0,0 +1,12 @@
+friend_list = ["부산에 살고 자주 보는 친구", "같은 학교를 다니는 친구"]
+info = ["서울로 떠난 중학교 친구", "게임에서만 보이는 친구", "행방불명된 친구"]
+print(friend_list)
+
+friend_list.append("굉장한 프로그래밍 실력을 가진 친구")
+print(friend_list)
+
+friend_list.append(info[0])
+friend_list.append(info[1])
+friend_list.append(info[2])
+print(friend_list)
+
