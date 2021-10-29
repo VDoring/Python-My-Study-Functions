8883a6750c712aa0d0623ea38d55cd371611e524
Doring <d0211468@kowon.dongseo.ac.kr>
2021-10-29 21:43:33 +0900

    메소드

---
+++ 나도코딩 유투브 파이썬 강의/기본편/9-4.py
@@ -0,0 +1,35 @@
+# 메소드
+
+class Unit:
+    def __init__(self, name, hp, damage): # __init__는 생성자이다. 자동으로 호출되는 부분.
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        print("{0} 유닛이 생성 되었습니다.".format(self.name))
+        print("채력 {0}, 공격력 {1}".format(self.hp, self.damage))
+
+# 공격 유닛
+class AttackUnit:
+    def __init__(self, name, hp, damage): # __init__는 생성자이다. 자동으로 호출되는 부분.
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+
+    def attack(self, location):
+        print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]"\
+            .format(self.name, location, self.damage))
+
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다.".format(self.name))
+
+# 파이어뱃 : 공격 유닛, 화염방사기
+firebat1 = AttackUnit("파이어뱃", 50, 16)
+firebat1.attack("5시")
+
+# 공격 2번 받는다고 가정
+firebat1.damaged(25)
+firebat1.damaged(25)
