208f3655cbd03466ccf68e7ee4c9d21ab39eccb3
Doring <d0211468@kowon.dongseo.ac.kr>
2021-11-02 22:06:27 +0900

    다중상속

---
+++ 나도코딩 유투브 파이썬 강의/기본편/9-6.py
@@ -0,0 +1,43 @@
+# 다중상속
+
+class Unit:
+    def __init__(self, name, hp): # __init__는 생성자이다. 자동으로 호출되는 부분.
+        self.name = name
+        self.hp = hp
+        
+
+# 공격 유닛
+class AttackUnit(Unit):
+    def __init__(self, name, hp, damage):
+        Unit.__init__(self, name, hp)
+        self.damage = damage
+
+    def attack(self, location):
+        print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]"\
+            .format(self.name, location, self.damage))
+
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다.".format(self.name))
+
+# 날 수 있는 기능을 가진 클래스
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]"\
+            .format(name, location, self.flying_speed))
+
+# 공중 공격 유닛 클래스
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, damage)
+        Flyable.__init__(self, flying_speed)
+
+# 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사
+valkyrle = FlyableAttackUnit("발키리", 200, 6, 5)
+valkyrle.fly(valkyrle.name, "3시")
