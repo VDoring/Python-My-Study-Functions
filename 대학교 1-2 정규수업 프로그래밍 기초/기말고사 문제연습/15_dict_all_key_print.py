698ffd97bd44c991843e18c1f84abb9ef2d57777
Doring <d0211468@kowon.dongseo.ac.kr>
2021-12-10 14:05:02 +0900

    딕셔너리의 모든 키 출력하기

---
+++ 대학교 1-2 정규수업 프로그래밍 기초/기말고사 문제연습/15_dict_all_key_print.py
@@ -0,0 +1,10 @@
+# 딕셔너리의 모든 키 출력하기
+
+animal = {'고양이': 'NyanNyan', '강아지': 'MungMung'}
+
+print(animal.keys()) # dict_keys(['고양이', '강아지'])
+
+print(animal.values()) # dict_values(['NyanNyan', 'MungMung'])
+
+
+# 10주차 1차시까지의 분량
