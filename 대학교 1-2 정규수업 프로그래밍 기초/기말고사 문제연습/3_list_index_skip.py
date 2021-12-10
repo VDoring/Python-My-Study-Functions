b43a5af4311398d45e8bf2b21e4e3262cf01af50
Doring <d0211468@kowon.dongseo.ac.kr>
2021-12-10 13:19:04 +0900

    리스트 인덱스 생략

---
+++ 대학교 1-2 정규수업 프로그래밍 기초/기말고사 문제연습/3_list_index_skip.py
@@ -0,0 +1,9 @@
+# 리스트 인덱스 생략
+
+letters = ['A', 'B', 'C', 'D', 'E', 'F']
+
+print(letters[:3])  # ['A', 'B', 'C']
+print(letters[3:])  # ['D', 'E', 'F']
+print(letters[:])   # ['A', 'B', 'C', 'D', 'E', 'F']
+print(letters[1:3]) # ['B', 'C']
+print(letters[0:3]) # ['A', 'B', 'C']
