f47c19f423fd69aeb8c6812ada2bee36e33630a8
Doring <d0211468@kowon.dongseo.ac.kr>
2021-12-10 13:17:03 +0900

    리스트 선언하기 & append를 사용하여 리스트 요소 추가하기

---
+++ 대학교 1-2 정규수업 프로그래밍 기초/기말고사 문제연습/1_list_append.py
@@ -0,0 +1,7 @@
+# 리스트 선언하기 & append를 사용하여 리스트 요소 추가하기
+
+computer = ["CPU", "RAM", "HDD", "SSD"]
+print(computer) # ['CPU', 'RAM', 'HDD', 'SSD']
+
+computer.append("PSU")
+print(computer) # ['CPU', 'RAM', 'HDD', 'SSD', 'PSU']
