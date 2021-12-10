97701c584cd305f3663294e790824d47b628e2da
Doring <d0211468@kowon.dongseo.ac.kr>
2021-12-10 14:04:34 +0900

    딕셔너리

---
+++ 대학교 1-2 정규수업 프로그래밍 기초/기말고사 문제연습/13_dict.py
@@ -0,0 +1,13 @@
+# 딕셔너리
+# 딕셔너리는 키와 값으로 구성된다. (key : value)
+
+animal = {}
+
+animal["고양이"] = "NyanNyan"
+print(animal) # {'고양이': 'NyanNyan'}
+
+animal["강아지"] = "MungMung"
+animal["달팽이"] = "..."
+print(animal) # {'고양이': 'NyanNyan', '강아지': 'MungMung', '달팽이': '...'}
+
+
