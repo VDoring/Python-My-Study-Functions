4ecd71d046838ba86e1df582e0204a14c38e79b0
Doring <d0211468@kowon.dongseo.ac.kr>
2021-12-12 10:29:45 +0900

    캐릭터 추가, 확인, 선택 시스템

---
+++ 대학교 1-2 정규수업 프로그래밍 기초/12_1.py
@@ -0,0 +1,126 @@
+# key = item 이름, value = 수량 
+inventory = {}
+
+# Todolist 1
+# 1. 아이템 추가시 수량도 인자로 받음
+# 2. inventory 전역함수이다. 인자로 받아서 처리하기
+def add_item(item, amount, t_inventory):
+    # 존재하면 1추가
+    if check_item(item, t_inventory):
+        t_inventory[item] += amount
+        print(item+"의 수량은 "+str(t_inventory[item])+"입니다.")
+    #존재하지않으면 추가하면서 수량은 1
+    else:
+        t_inventory[item] = amount
+        print(item+"이 추가되었습니다.")
+
+#기존의 수량을 모두 버림(수량 0)
+#존재하지 않으면 무시
+def remove_item(item, t_inventory):
+    if check_item(item, t_inventory):
+        t_inventory[item] = 0
+        print(item+"의 수량이 0이 되었습니다.")
+    else:
+        print(item+"이 존재하지 않습니다.")
+#새로운 함수, 포션 사용
+#존재하는 아이템을 수량 1 빼기
+def consume_item(item):
+    pass
+
+
+def check_item(item, t_inventory):
+    return item in t_inventory
+
+def print_menu():
+    print("0. 끝내기")
+    print("1. 아이템 추가")
+    print("2. 아이템 삭제")
+    print("3. 아이템 확인")
+    print("4. 아이템 사용")
+
+# Todolist 2
+# while문을 아이템 다루기 함수로 구현
+def use_item():
+    while True:
+        print_menu()
+        option = int(input("메뉴 번호를 입력하세요)"))
+        if option == 0:
+            print("종료합니다.")
+            break
+        elif option == 1:
+            new_item = input("아이템을 입력하세요.)")
+            amount = int(input("수량을 입력하세요.)"))
+            # Todolist 1'
+            # 함수 인자 추가하는 기능 구현시 호출 부분 수정 필
+            add_item(new_item, amount, inventory)
+        elif option == 2:
+            eliminated_item = input("아이템을 입력하세요.)")
+            remove_item(eliminated_item, inventory)
+        elif option == 3:
+            print(inventory)
+        elif option == 4:
+            pass
+        else:
+            print("잘못된 번호를 입력하셨습니다.")
+
+
+# key = item 이름, value = 수량 
+inventory = {}
+
+# todolist 3
+# 캐릭터 만들기
+character = {}
+def new_character(name, t_character):
+    if check_character(name, t_character):
+        print("이미 존재하는 캐릭터의 이름입니다.")
+    else:
+        inventory = {}
+        t_character[name] = inventory
+        
+def check_character(name, t_character):
+    return name in t_character
+
+def print_characterMenu():
+    print("0. 끝내기")
+    print("1. 캐릭터 추가")
+    print("2. 캐릭터 이름출력")
+    print("3. 캐릭터 선택")
+    print("4. 캐릭터 인벤토리 조작")
+    
+while True:
+    print_characterMenu()
+    option = int(input("메뉴를 선택해주세요.)"))
+    if option == 0:
+        print("종료되었습니다.")
+        break
+    elif option == 1:
+        name = input("캐릭터 이름을 입력하세요.)")
+        new_character(name, character)
+    elif option == 2:
+        i = 1
+        print("################")
+        for name in character.keys():
+            print(str(i)+". "+name)
+            i += 1
+        print("################")
+    elif option == 3:
+        temp_name = input("선택할 캐릭터의 이름을 입력해주세요.)")
+        if check_character(temp_name, character):
+            select_character = temp_name
+            print(select_character + "이 선택되었습니다.")
+        else:
+            print(temp_name+"은 존재하지 않는 캐릭터입니다.")
+        
+    
+# 캐릭터 이름으로 식별
+# 캐릭터 인벤토리
+# 캐릭터 장착기능
+
+
+
+
+
+
+
+
+
